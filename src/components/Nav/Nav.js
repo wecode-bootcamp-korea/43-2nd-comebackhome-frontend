e7297e7736b9430f9e08b883a002653db30146f0
kitae <kitae@kitaeui-MacBookPro.local>
2023-03-27 19:27:54 +0900

    ADD:nav ui 구현
    ADD:nav 구현 완료
    
    Modify: 수정요청 반영완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as S from './NavStyle';
+import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
-  return <div />;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    alert('로그아웃 되었습니다.');
+  };
+  return (
+    <S.Header>
+      <S.Container>
+        <S.TitleLogo>
+          <Link to="/" className="LinkLogo">
+            <img
+              src="/images/comebackhome2.png"
+              alt="콤배콤"
+              className="ImgLogo"
+            />
+          </Link>
+        </S.TitleLogo>
+        <S.NavMenu>
+          <S.Li
+            onClick={() => {
+              navigate('/homepartylist');
+            }}
+          >
+            <S.A>집들이</S.A>
+          </S.Li>
+          <S.Li
+            onClick={() => {
+              navigate('/productlist');
+            }}
+          >
+            <S.A>쇼핑</S.A>
+          </S.Li>
+        </S.NavMenu>
+        <S.Navcontents>
+          {true ? (
+            <S.Li>
+              <S.Astyled>
+                <S.ProfileImg>
+                  <img src="/images/thumbnail.jpg" alt="프로필" />
+                </S.ProfileImg>
+                닉네임
+              </S.Astyled>
+              <S.Logout onClick={handleLogout}>로그아웃</S.Logout>
+            </S.Li>
+          ) : (
+            <S.Li
+              onClick={() => {
+                navigate('/signin');
+              }}
+            >
+              <S.Astyled>로그인</S.Astyled>
+            </S.Li>
+          )}
+          <S.Li
+            onClick={() => {
+              navigate('/signin');
+            }}
+          >
+            <S.Astyled>회원가입</S.Astyled>
+          </S.Li>
+          <S.CartImageBox>
+            <Link to="/cart">
+              <S.CartImage src="/images/add-cart.png" alt="장바구니" />
+            </Link>
+          </S.CartImageBox>
+        </S.Navcontents>
+      </S.Container>
+    </S.Header>
+  );
 };
 
 export default Nav;
