2300a1850edb8484bc22e3a79355bfb38818c4e7
eunbinmoon <bin1109a@gmail.com>
2023-03-28 20:27:32 +0900

    쇼핑리스트 페이지 UI구현

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Width } from '../../styles/common';
+import Product from '../../components/Product/Product';
+import * as S from './ProductListStyle.js';
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const [productItem, setProductItem] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/ProductItem.json')
+      .then(res => res.json())
+      .then(data => {
+        setProductItem(data);
+      });
+  }, []);
+
+  return (
+    <Width>
+      <S.TitleBox>
+        <S.ShoppingCategory>일상을 바꾸는 물건들,</S.ShoppingCategory>
+        <S.TitleLogo src="images/dinnertable2.png" alt="물건사진" />
+      </S.TitleBox>
+      <S.DisplayCenter>
+        {productItem.map(({ id, thumbnail, productName, price, discount }) => (
+          <Product
+            key={id}
+            id={id}
+            thumbnail={thumbnail}
+            productName={productName}
+            price={price}
+            discount={discount}
+          />
+        ))}
+      </S.DisplayCenter>
+    </Width>
+  );
 };
 
 export default ProductList;
---
+++ src/pages/ProductList/ProductListStyle.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const ShoppingCategory = styled.div`
+  font-family: 'IBMPlexSansKR-Regular';
+  color: black;
+  font-size: 30px;
+  line-height: 2px;
+  text-shadow: 0 0 3vw #afd485;
+  text-align: center;
+  font-weight: 700;
+`;
+
+export const TitleLogo = styled.img`
+  width: 100px;
+  margin: 25px 20px 0 575px;
+`;
+
+export const DisplayCenter = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  column-gap: 60px;
+`;
+
+export const TitleBox = styled.div`
+  width: 1256px;
+  margin-top: 20px;
+  margin-bottom: 45px;
+`;
